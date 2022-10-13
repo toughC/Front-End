0d9109c6be4972cd028ed71d019befe6bcebcc0c
leejyohan <86225337+leejyohan@users.noreply.github.com>
2022-10-13 21:57:11 +0900

    css  수정

--- src/components/InfoShare.vue
+++ src/components/InfoShare.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="info-title">
-    해운대구 위치 아이콘
-    <MapIcon/>
+    해운대구
+    <div class="info-map-icon"></div>
+    <hr />
   </div>
-  <div>
-    정보 공유 스위치
+  <div style="margin-bottom:20px">
+    <label class="switch-button">
+      <input type="checkbox" />
+      <span class="onoff-switch"></span>
+    </label>
+    <span class="switch-text">정보공유</span>
   </div>
-  <div>
-    이것은 행사 이름입니다 (지금당장!)
+<div v-for="i in ex" :key="i">
+
+  <div style="margin-bottom:20px">
+    이것은 행사 이름입니다 
+    <div class="date-button">지금당장</div>
   </div>
-  <div>
+  <div class="info-map" style="margin-bottom:20px">
     위치 지도
   </div>
   <div>
     관련 내용입니다
   </div>
+  <hr />
+</div>
 </template>
   
 <script>
@@ -22,17 +32,17 @@ export default {
   name: 'InfoShare',
   data() {
     return {
-
+      ex : [ 1,2,3]
     }
   },
   components: {
-    MapIcon
+
   }
 }
 </script>
   
 <style>
-  @import '../css/Info.css';
+@import '../css/Info.css';
 </style>
   
   
